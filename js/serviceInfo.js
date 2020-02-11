7c86a5337f32812e18a0ed9a080b25e52dc7d6fb
Alastair McGilloway <alastairmcgillowaY@hotmail.com>
2020-02-11 16:04:18 +0000

    Updates Modals

---
+++ js/serviceInfo.js
@@ -0,0 +1,60 @@
+const ServiceDescription =
+{
+  bookkeeping:
+  {
+    title: 'Bookeeping',
+    description: "<p>Keeping business records is very important as these are the basis on which we prepare the finalised accounts. In the event of an enquiry, HMRC may request to see your paperwork to supportthe submitted accounts. <br /> <br /> We offer to process and file your paperwork in a timely and organised manner, so your records are  kept up to date and easy to access. This also helps us in getting to know and understand your business better, and be able to provide the best service tailored to your needs. <br /> <br /> Regular bookkeeping also means that your annual accounts can be finalised soon after the year end. <br /></p>"
+  },
+    partnerships: {
+    title: 'Partnerships',
+    description: "<p> If you are going into business with somebody else, you can setup a ‘partnership’. This can either be an ordinary, limited or alimited liability partnership. <br /><br /> As with self-employment,each partner is liable to income tax and National Insurance, andmust submit tax returns to HMRC. A tax return must also besubmitted for the partnership itself. The partnership may alsobe liable for VAT depending on the turnover. <br /> <br />We willprepare the annual accounts, submit the tax returns, andcalculate the tax and NI liabilities. <br /></p>"
+  },
+    businessAccounts: {
+    title: 'Business Accounts',
+    description: "<p> Whether self-employed or a limited company, we will prepare annual business accounts. For self-employed these generally run from 6 April – 5 April, while companies will have different year ends. <br /> <br />For limited companies annual accounts must be submitted to both Companies House and HMRC. We aim to finalise accounts within a few months of your company year end, so that the information is up to date and filing deadlines are met. <br /> <br /> The accounts can also be used as support if you are applying for a business loan, or entering a finance agreement. <br /> <br /> We also offer management account preparation, either on a monthly or quarterly basis, so you can assess your business performance throughout the year. <br /></p>"
+  },
+    companyForm: {
+    title: 'Company Formation',
+    description: "<p> A popular option for running your own business is to set up as a limited company. There are many benefits to having a company, and we can assess if this would best suit you, depending on your own circumstances. <br /><br /> We provide an incorporation service, dealing with the registration process at Companies House, and also offer company secretarial services including the annual filing with Companies House and receiving all official mail on your behalf, as the company’s registered office. <br /></p>"
+  },
+    construction: {
+    title: 'Construction',
+    description: "<p> This scheme is a method of collecting money in advance towards the tax and National Insurance liabilities of subcontractors. The deduction is made by contractors and paid to HMRC. <br /><br /> For Contractors – If you hire subcontractors for construction work, you must register as a contractor with HMRC. Monthly returns must be submitted to HMRC reporting the payments and deductions for each subcontractor. We will register you as a contractor, and verify each subcontractor with HMRC, submit the monthly returns, and advise you of the net payments and tax deductions. <br /> <br />For Subcontractors – At the end of each tax year, we will calculate any refund of the total CIS deductions made in that year, for both self-employed and limited companies. If a refund is due we will then make a claim with HMRC for this on your behalf. <br /></p>"
+  },
+    corpTax: {
+    title: 'Corporation Tax',
+    description: "<p> Limited companies are liable to corporation tax, as opposed to income tax for individuals. This tax is calculated on the company profits. <br /><br /> As part of the company formation process, HMRC are notified of the incorporation and issue the company with its own unique tax reference. A tax return must be completed which includes the corporation tax calculation. <br /><br /> We prepare and submit the company tax return along with the annual accounts, keeping your company compliant with HMRC regulations. <br /></p>"
+  },
+    inheritance: {
+    title: 'Inheritance',
+    description: "<p> Inheritance Tax is charged on personal net worth on death. Assets transferring to a surviving spouse are exempt but Inheritance Tax becomes a serious risk to the transfer of assets to children. <br /><br /> The rate of 40% and with a starting net value at only £325,000 makes this tax one that has to be considered well in advance and any possible tax planning carried out. <br /></p>"
+  },
+    mgd: {
+    title: 'Machine Games Duty',
+    description: "<p> If your business offers Machine Games (such as those found in pubs and clubs), you may be liable for Machine Games Duty which is a tax on machines which offer cash prizes. Tax is calculated on your net takings from the machine. <br /><br /> We can discuss if this scheme applies to your business, and if so we will register you with HMRC, prepare and submit the quarterly returns and calculate any tax due. <br /></p>"
+  },
+    paye: {
+    title: 'Pay As You Earn',
+    description: "<p> For Employers – As part of our payroll service, we operate the PAYE scheme for your employees, advising you of the tax and national insurance deductions and net wage for each employee. The tax &amp; NI deductions must be reported and paid to HMRC on a monthly basis and we will advise you of the total amounts each payday. <br /><br /> For Employees - PAYE is the method of paying income tax and National Insurance through your wages as an employee. <br /><br /> HMRC notify your employer of the tax code to use in order to calculate the tax deductions. This tax code will depend on your own circumstances and can vary depending on factors such as previous years’ tax, or benefits you receive such as a company car, medical insurance etc. <br /><br /> You may be able to claim relevant job expenses, depending on the nature of your work. We offer to submit a claim for this to HMRC on your behalf. <br /></p>"
+  },
+    payroll: {
+    title: 'Payroll',
+    description: "<p> If your business is at the stage where you need to hire employees, we offer a payroll service to help you make the transition to becoming an employer. <br /><br /> Anyone can be an employer, whether self-employed or a limited company. We will register your business with HMRC as an employer and process your payroll on a regular basis, providing payslips (paper or by email), advising of your employees’ net wages, and any tax &amp; National Insurance deductions made under the PAYE scheme.<br /> <br /> As part of the service we also process forms such as P45s and P60s, and calculate statutory payments including sick pay and maternity pay. <br /> <br />Real Time Information (RTI) <br /> <br />RTI was introduced by HMRC in the 2013/14 tax year and was a major change to the payroll legislation. <br /> <br />Employers must submit returns to HMRC on or before each payday, with details of starters, leavers, and tax &amp; NI deductions etc. <br /><br /> We will submit the returns with every pay-run to ensure you are compliant with this legislation. <br /></p>"
+  },
+    pensions: {
+    title: 'Pensions',
+    description: "<p> Workplace Pension schemes<br /><br /> Automatic enrolment is a new law which means that every employer must automatically enrol employees into a workplace pension scheme if they meet certain criteria. <br /><br /> Every employer has a different ‘staging date’, i.e. the date Automatic Enrolment begins, and we will work closely with you to ensure you meet the requirements of this new </p>"
+  },
+    selfAssess: {
+    title: 'Self Assessment',
+    description: "<p> Self-assessment is not simply for those who are self-employed. There are many circumstances in which you may be required to complete a self-assessment tax return, including the following: <br /><br /> Rental Income -  You have to notify HMRC if you are renting out one or more properties, as any profits could be liable to tax and you may need to submit annual tax returns to declare this income. <br /><br /> Non-Resident Landlords – you must still declare your rental income to HMRC and submit tax returns for rental income from your UK property<br /><br /> Capital Gains - Any profit you make from selling an asset could be liable to capital gains tax.  We can assess if you have to pay capital gains tax and complete a tax return reporting the gain and relevant tax liability. <br /><br /> Foreign Income - You may need to pay tax on foreign income such as wages from working abroad, rental income, foreign investments or savings interest. <br /><br /> Dividends - From April 2016 new rules on the taxation of dividends were introduced. Any dividends over £5,000 will be liable to income tax and must be declared to HMRC. <br /><br /> If you are unsure about whether you should be completing a tax return please contact us.<br /></p>"
+  },
+    selfEmp: {
+    title: 'Self Employed',
+    description: "<p> If you have started self-employment you must register with HM Revenue &amp; Customs. We will take the stress out of this by dealing with the registration process. <br /> <br /> Self-employed must complete tax returns and are liable to income tax and National Insurance on business profits. They may also have to register for VAT, depending on the level of turnover. <br /><br />We will prepare annual accounts which form the basis of the tax return, calculate any tax and National Insurance due, and submit your tax return to HMRC each year. <br /> </p>"
+  },
+    vat: {
+    title: 'VAT',
+    description: "<p>Value Added Tax<br /> VAT can be complicated and we offer areliable service to help ease the stress of becoming a VATregistered business. <br /><br /> There are different schemesavailable to account for VAT, and we will decide which schemewould be most beneficial, depending on your business. <br /><br /> Weprocess the registration with HMRC, prepare and submit the VATreturns, and advise you of any VAT payment due. <br /> </p>"
+  }
+}
